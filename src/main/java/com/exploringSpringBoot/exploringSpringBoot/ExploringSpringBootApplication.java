597a4a2cb9f9a6528a61d3a3499d3dd50b1c3e21
NogueraMateo <mateo.edu.co@gmail.com>
2024-11-19 10:07:12 -0500

    First commit

---
+++ src/main/java/com/exploringSpringBoot/exploringSpringBoot/ExploringSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.exploringSpringBoot.exploringSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploringSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exploringSpringBoot/exploringSpringBoot/ExploringSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploringSpringBoot.exploringSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploringSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
